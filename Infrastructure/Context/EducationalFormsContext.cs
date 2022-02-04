d1a4108aa96bbc1b74e08372dfbfd0a8c34711b8
Farzad Sepehr <f.sepehrjoo@didar.me>
2022-02-05 00:51:50 +0330

    Create first migration and dbcontext

---
+++ Infrastructure/Context/EducationalFormsContext.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class EducationalFormsContext : DbContext
+{
+    public EducationalFormsContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public Student Students { get; set; }
+    public Skill Skills { get; set; }
+    public Consulting Consultings { get; set; }
+
+}
