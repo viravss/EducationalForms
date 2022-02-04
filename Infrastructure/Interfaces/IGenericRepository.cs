ada010e0d6156e26baefa33a33152cb10fbe064b
Farzad Sepehr <f.sepehrjoo@didar.me>
2022-02-05 01:39:41 +0330

    create generic repositories

---
+++ Infrastructure/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Infrastructure.Interfaces;
+
+public interface IGenericRepository<T>
+{
+    public Task<T> GetAsync(int id);
+    public IQueryable<T> GetAll();
+    public IQueryable<T> Where(Expression<Func<T, bool>> expression);
+    public Task AddAsync(T entity);
+    public T Update(T entity);
+    public void Delete(T entity);
+}
---
+++ Infrastructure/Interfaces/IStudentRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Models;
+
+namespace Infrastructure.Interfaces;
+
+public interface IStudentRepository : IGenericRepository<Student>
+{
+
+}
